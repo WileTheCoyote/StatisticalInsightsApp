3cbbd4e87426413db99361e1a1918d0aa9027d9e
Jake White <jakewhite@Jakes-MacBook-Pro-2.local>
2014-12-08 05:19:17 -0700

    printed text color template at bottom of code

--- StatisticalInsights.java
+++ StatisticalInsights.java
@@ -356,12 +356,29 @@ public class StatisticalInsights {
         }
         
         gameStatsUrl = "http://statsheet.com" + gameStatsUrl;
-        
-        
+                
+    /*************************** Text color manipulator variables *****************************************************/
+        String ANSI_RESET = "\u001B[0m";        // called at end of string to reset to original text color
+        String ANSI_BLACK = "\u001B[30m";
+        String ANSI_RED = "\u001B[31m";         // called at begining of string to print text as red
+        String ANSI_GREEN = "\u001B[32m";
+        String ANSI_YELLOW = "\u001B[33m";
+        String ANSI_BLUE = "\u001B[34m";
+        String ANSI_PURPLE = "\u001B[35m";
+        String ANSI_CYAN = "\u001B[36m";
+        String ANSI_WHITE = "\u001B[37m";
+    /********************************************************************************************************************/
+
+        // testing color 
+
+        System.out.println(ANSI_RED + "This Text is red??" + ANSI_RESET);
+        System.out.println("Hopefully this isn't");        
         
 
     }
 }
+
+
 
 
 
